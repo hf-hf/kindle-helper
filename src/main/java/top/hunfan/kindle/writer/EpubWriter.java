0580ed41e29e3cf14e4e660a59e0426418d5253b
hf-hf <553527481@qq.com>
2019-02-02 12:00:02 +0800

    TODO generator Epub file

---
+++ src/main/java/top/hunfan/kindle/writer/EpubWriter.java
@@ -0,0 +1,17 @@
+package top.hunfan.kindle.writer;
+
+import top.hunfan.kindle.domain.Book;
+
+/**
+ * epub文件生成
+ * @author hefan
+ * @date 2019/2/2 11:38
+ */
+public class EpubWriter implements Writer{
+
+    @Override
+    public void generate(Book book, String savePath) {
+
+    }
+
+}
